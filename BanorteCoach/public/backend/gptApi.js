f693f5a8876f1102fc23204225057044ea32c66b
Oscar-gg <oscar.arreola.jr@gmail.com>
2023-09-23 18:42:48 -0600

    feature(new prompts): public backend

--- BanorteCoach/public/backend/gptApi.js
+++ BanorteCoach/public/backend/gptApi.js
@@ -1,27 +1,111 @@
 import { OpenAI } from "openai";
+import data from "../data/fondos.json";
 
-const openai = new OpenAI({apiKey: "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", dangerouslyAllowBrowser: true});
+const openai = new OpenAI({
+  apiKey: "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",
+  dangerouslyAllowBrowser: true,
+});
 
 export default async function (animal) {
+  const message = [{ role: "user", content: generatePrompt(animal) }];
+
+  return completionConfig(message);
+}
+
+const completionConfig = async (messages, temperature = 0.6) => {
   const completion = await openai.chat.completions.create({
     model: "gpt-3.5-turbo",
-    messages: [{ role: "user", content: generatePrompt(animal) }],
-    temperature: 0.6,
+    messages: messages,
+    temperature: temperature,
   });
-  console.log(completion);
-  
   return completion;
+};
+
+export async function askInputData(question) {
+  const extraDescription =
+    "Te preguntan acerca del llenado de datos para perfilación de usuario. Responde con términos fáciles de comprender para el usuario.";
+  const message = [
+    {
+      role: "system",
+      content: generateSystemContext(false, extraDescription, false),
+    },
+    {
+      role: "user",
+      content: question,
+    },
+  ];
+  return completionConfig(message);
 }
 
-function generatePrompt(animal) {
-  const capitalizedAnimal =
-    animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  return `Suggest three names for an animal that is a superhero.
+export async function giveInvestOptions(userContext) {
+  const investOptions = JSON.stringify(data);
+  const extraDescription = `Las opciones de inversión disponibles
+  son las siguientes: ${investOptions}.`;
+  const responseType =
+    "una lista de opciones de inversión, cada una con el nombre de la opción y el porcentaje de relación con el perfil del usuario. Retorna la lista separada por ';'";
+  const message = [
+    {
+      role: "system",
+      content: generateSystemContext(false, extraDescription, responseType),
+    },
+    {
+      role: "user",
+      content: userContext,
+    },
+  ];
 
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: ${capitalizedAnimal}
-Names:`;
+  return completionConfig(message);
 }
+
+export async function explainSelectedInvestments(investOptions) {
+  const extraDescription = `Te dan el perfil de un usuario y sus inversiones. Explica por qué las opciones son adecuadas para el usuario.`;
+  const responseType =
+    "una lista de explicaciones, cada una con el titulo de la inversion un ':' y la explicación. Retorna la lista separada por ';'";
+  const message = [
+    {
+      role: "system",
+      content: generateSystemContext(false, extraDescription, responseType),
+    },
+    {
+      role: "user",
+      content: investOptions,
+    },
+  ];
+
+  return completionConfig(message);
+}
+
+const generateSystemContext = (
+  userDescription = false,
+  extraDescription = false,
+  responseType = false
+) => {
+  let promptString = "Eres asesor de inversiones.";
+  if (userDescription)
+    promptString += `Estás hablando con: ${userDescription}, responde en términos apropiados para el usuario.`;
+
+  if (extraDescription) promptString += extraDescription;
+  if (responseType)
+    promptString += `Tu respuesta tiene que ser ${responseType}.`;
+
+  return promptString;
+};
+
+// function generatePrompt(animal) {
+//   const capitalizedAnimal =
+//     animal[0].toUpperCase() + animal.slice(1).toLowerCase();
+//   return `Suggest three names for an animal that is a superhero.
+
+// Animal: Cat
+// Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
+// Animal: Dog
+// Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
+// Animal: ${capitalizedAnimal}
+// Names:`;
+// }
+
+export const extractLastMessage = (response) => {
+  const finalMessage = response.choices[response.choices.length - 1].message;
+  return finalMessage.content;
+}
+
--- BanorteCoach/src/App.jsx
+++ BanorteCoach/src/App.jsx
@@ -1,30 +1,94 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import Gpt from "../public/backend/gptApi";
+import Gpt, { askInputData, giveInvestOptions, extractLastMessage} from "../public/backend/gptApi";
 
 // pages
 import LandingPage from "./pages/LandingPage";
 
+// import { connection, getUsers } from "../public/backend/db";
+
+
 function App() {
-  
   const [res, setRes] = useState("Def");
-  const handleButtonClick = async () => {
+  const [res2, setRes2] = useState("Def2");
+  const [res3, setRes3] = useState("Def3");
+
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     try {
+  //       const users = await getUsers();
+  //       console.log(users);
+  //     } catch (error) {
+  //       console.error("Error fetching users:", error);
+  //     }
+  //   }
+
+  //   fetchData();
+
+  //   // Don't forget to clean up if necessary (e.g., closing a database connection)
+  //   connection.end();
+  // }, []); 
+  
+  const [text, setText] = useState("");
+  const [text2, setText2] = useState("");
+  const [text3, setText3] = useState("");
+
+  const handleChange = (event) => {
+    setText(event.target.value);
+  };
+
+  const handleChange2 = (event) => {
+    setText2(event.target.value);
+  };
+
+  const handleChange3 = (event) => {
+    setText3(event.target.value);
+  };
+
+  const handleQuestion = async () => {
     try {
-      const res = await Gpt("Elephant");
-      console.log(res);
-      setRes(JSON.stringify(res));
+      const res = await askInputData(text);
+      const texto = res.choices[0];
+      setRes2(texto);
     } catch (error) {
       console.log(error);
     }
+  };
 
-  }
+  const handleInvestOptions = async () => {
+    try {
+      const res = await giveInvestOptions(text3);
+      // const texto = res.choices[0].message.content;
+      console.log(res);
+      const texto = extractLastMessage(res);
+      console.log(res)
+      console.log(texto)
+      setRes3(texto);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-  return (  
+  return (
     <>
       <div>
-        <button onClick={handleButtonClick}> Hacer request</button>
+        <h1>Pregunta de llenado de datos:</h1>
+        <h2>Respuesta: </h2>
+        <p>{res2}</p>
+
+        <input type="text" value={text2} onChange={handleChange2} />
+        <button onClick={handleQuestion}> Preguntar llenado</button>
+
+        <h1>Pregunta opciones de inversion:</h1>
+        <h2>Respuesta:</h2>
+        <h2>{res3}</h2>
+        <input type="text" value={text3} onChange={handleChange3}/>
+        <button onClick={handleInvestOptions}> Llamar opciones inversion</button>
+
+        {/* <button onClick={handleButtonClick}> Hacer request</button> */}
+
         <p>{res}</p>
       </div>
     </>
