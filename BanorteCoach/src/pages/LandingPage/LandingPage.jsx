daee3c406be8bc5d4446a8c0bcb6146942898cf8
Oscar-gg <oscar.arreola.jr@gmail.com>
2023-09-23 21:30:28 -0600

    feat(added  page info, missing styling): landing page

--- BanorteCoach/src/pages/LandingPage/LandingPage.jsx
+++ BanorteCoach/src/pages/LandingPage/LandingPage.jsx
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom";
-import style from "./landingStyle.js";
+import style, { style2 } from "./landingStyle.js";
+import bannerExplicacion from "../../assets/LandingImagen.jpg";
+import bannerExplicacion2 from "../../assets/torre-koi-png.png";
 
 export default function LandingPage() {
   return (
@@ -17,6 +19,48 @@ export default function LandingPage() {
               Encuentra tu inversión ideal
             </button>
           </Link>
+        </div>
+      </div>
+
+      <div style={style2.banner}>
+        <div style={style2.mainTxtWrapper}>
+          <h2 style={style2.mainTittle}>¿Qué es BanCoach?</h2>
+          <div style={style2.imageContainer}>
+            <img src={bannerExplicacion} />
+            <div style={{ width: "50%" }}>
+              <p>
+                BanCoach es un sistema que te facilita encontrar las mejores
+                opciones de inversión dependiendo de tu perfil de inversionista.
+              </p>
+
+              <p>
+                Primero empieza con una perfilación de tu situación financiera,
+                para después recomendarte las mejores opciones de inversión que
+                concuerdan con tu perfil.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div style={style2.banner}>
+        <div style={style2.mainTxtWrapper}>
+          <h2 style={style2.mainTittle}>Más fácil que nunca</h2>
+          <div style={style2.imageContainer}>
+            <div style={{ width: "50%" }}>
+              <p>
+                No dudes en preguntarle a BanCoach todas tus dudas sobre
+                inversiones. Te responderá al momento con la información más
+                actualizada.
+              </p>
+
+              <p>
+                Explora e informate de una gran cantidad de fondos de inversión
+                como nunca antes. Vuelvte un conocedor de las inversiones y da
+                seguimiento de manera efectiva.
+              </p>
+            </div>
+            <img src={bannerExplicacion2} />
+          </div>
         </div>
       </div>
     </div>
--- BanorteCoach/src/pages/LandingPage/landingStyle.js
+++ BanorteCoach/src/pages/LandingPage/landingStyle.js
@@ -17,8 +17,6 @@ const style = StyleSheet.create({
     backgroundRepeat: 'no-repeat',
     backgroundSize: 'cover', // You can adjust this based on your requirements
     backgroundColor: 'rgba(0, 0, 0, 0.5)', // Adjust the opacity (0.5 for 50% darkness)
-
-
     },
     mainTxtWrapper: {
         marginLeft: 150,
@@ -54,5 +52,60 @@ const style = StyleSheet.create({
     },
 
 });
+
+export const style2 = StyleSheet.create({
+
+    banner: {
+        width: '100vw',
+        height: '100vh',
+        alignItems: "center",
+        display: 'flex', // Use flex for layout
+        //add backgorud image
+        // Add background image using template literals
+    backgroundPosition: 'center', // Adjust to your preference
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover', // You can adjust this based on your requirements
+    backgroundColor: 'rgba(0, 0, 0, 0.5)', // Adjust the opacity (0.5 for 50% darkness)
+    },
+    imageContainer: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    mainTxtWrapper: {
+        marginLeft: 150,
+        width: 1200,
+        height: 400,
+        display: 'flex', // Use flex for layout
+        flexDirection: 'column', // or 'row' based on your design
+        justifyContent: 'center', // or other appropriate value
+    },
+    mainTittle: {
+        marginTop: 270,
+        marginLeft: "auto",
+        marginRight: "auto",
+        fontSize: 70,
+        color: "white",
+        fontStyle: "Bold",
+        textAlign: "center",
+    },
+    startButton: {
+        margin: 'auto',
+        marginLeft: '34vw',
+        marginTop: 50,
+        width: 200,
+        height: 70,
+        backgroundColor: colors.primary,
+        color: "white",
+        alignItems: "center",
+        justifyContent: "center",
+        textAlign: "center",
+        borderRadius: 5,
+        borderWidth: 2,
+    },
+
+});
+
 
 export default style;
