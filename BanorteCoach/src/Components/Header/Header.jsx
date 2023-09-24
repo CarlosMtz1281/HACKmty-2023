89b37add3d47a6ee04115b09679a48393e85803b
GermanS04 <german.salasmar@gmail.com>
2023-09-24 04:38:03 -0500

    feat(Section and User Header):Components

--- BanorteCoach/src/Components/Header/Header.jsx
+++ BanorteCoach/src/Components/Header/Header.jsx
@@ -3,13 +3,19 @@ import { Link } from 'react-router-dom';
 import './styles'
 import { styles } from './styles';
 import logo from '../../assets/LogoBanorte.png'
+import userimg from '../../assets/userimg.jpg'
+import VerticalLineDivider from './Divider';
 
 const Header = () => {
     return(
         <div className="Header" style={styles.Header}>
-            <div className="banorte">
+            <div className="banorte" style={styles.banorte}>
                 <img src={logo} style={styles.logo}/>
-            </div>
+                <VerticalLineDivider />
+                <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'left', alignItems: 'left', gap: "0px"}}>
+                <div>COACH</div><div>FINANCIERO</div>
+                </div>
+            </div> 
             <div className="paginas" style={styles.paginasHeader}>
                 <div className="LandingPage">
                     <Link to="/LandingPage" style={styles.text}>LANDING</Link>
@@ -19,6 +25,14 @@ const Header = () => {
                 </div>
                 <div className="InversionesPage">
                     <Link to="/InversionesPage" style={styles.text}>INVERSIONES</Link>
+                </div>
+            </div>
+            <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'right', alignItems: 'right', gap: "20px", marginRight: '-10rem'}}>
+                <div style={{fontSize: '17px'}}>
+                    Jose
+                </div>
+                <div>
+                    <img src={userimg} style={{width: '2.7rem', borderRadius: '50%'}}/>
                 </div>
             </div>
         </div>
