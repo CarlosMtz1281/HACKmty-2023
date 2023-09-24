aa05eb2b170890a61acc21c71ecd5b889e39150e
Carlos Martinez <carlosh.mtz@hotmail.com>
2023-09-23 20:54:35 -0600

    feat(inversiones page layout): inversiones page

--- BanorteCoach/src/pages/CoachPage/coachStyle.js
+++
@@ -1,9 +0,0 @@
-import { StyleSheet } from "react-native-web";
-import {colors} from '../../assets/constants.js'
-
-import bannerImg from '../../assets/Skyline2.jpg';
-const style = StyleSheet.create({ 
-
-})
-
-export default style
--- BanorteCoach/src/pages/InversionesPage/inversionesStyle.js
+++ BanorteCoach/src/pages/InversionesPage/inversionesStyle.js
@@ -2,7 +2,39 @@ import { StyleSheet } from "react-native-web";
 import {colors} from '../../assets/constants.js'
 
 import bannerImg from '../../assets/Skyline2.jpg';
-const style = StyleSheet.create({ 
+const style = StyleSheet.create({
+
+    banner: {
+        width: '100vw',
+        height: '100vh',
+        alignItems: "center",
+        display: 'flex', // Use flex for layout
+        //add backgorud image
+        // Add background image using template literals
+        backgroundImage: `url(${bannerImg})`,
+        backgroundPosition: 'center', // Adjust to your preference
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover', // You can adjust this based on your requirements
+        backgroundColor: 'rgba(0, 0, 0, 0.5)', // Adjust the opacity (0.5 for 50% darkness)
+
+
+    },
+    mainWrapper: {
+        marginLeft: 0,
+
+        width: '90vw',
+        height: '85vh',
+        marginLeft: '5vw',
+        marginTop: '8vh',
+        borderRadius: 5,
+        backgroundColor: colors.black,
+        opacity: 0.8,
+        //place in center
+        alignItems: "center",
+        display: 'flex', // Use flex for layout
+        //column
+        flexDirection: 'column', // or 'row' based on your design
+    },
     // Add title text on top and center just below header
     mainContainer:{
         width: "100vw",
@@ -16,17 +48,37 @@ const style = StyleSheet.create({
         alignItems: "center",
     },
     mainTitle: {
-        marginTop: 70,
-        fontSize: 55,
-        color: "black",
+        marginTop: '5vh',
+        fontSize: 40,
+        color: colors.white,
         fontStyle: "Bold",
         textAlign: "center",
     },
+
+    secondaryWrap: {
+        display: 'inline-block',
+        height: '65vh',
+        width: '15vw',
+        marginLeft: '-70vw',
+        backgroundColor: colors.background,
+    },
+
+    primaryWrap: {
+        display: 'inline-block',
+        height: '65vh',
+        width: '65vw',
+        marginLeft: '15vw',
+        marginTop: '-65vh',
+        backgroundColor: colors.background,
+    },
+
+
     // Put in the center page the container of invest
     investContainer: {
         width: "100vw",
         height: "70vh",
         alignItems: "center",
+        justifyContent: "center",
         display: 'flex', // Use flex for layout
         //column
         flexDirection: 'row', // or 'row' based on your design
